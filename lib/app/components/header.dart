4e84a8b0053030d4149996cadeb97bcdd242d7f4
Dsribass <ds_ribas@hotmail.com>
2021-06-17 10:14:28 -0300

    Update header

--- lib/app/components/header.dart
+++ lib/app/components/header.dart
@@ -1,33 +1,24 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class Header extends StatelessWidget {
+class Header extends StatelessWidget {  
   final String title;
-
+  
   const Header({Key? key, required this.title}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Container(
       padding: EdgeInsets.symmetric(horizontal: 40.0, vertical: 20.0),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Text(
+      child: Text(
             title,
+            textAlign: TextAlign.left,
             style: TextStyle(
               fontSize: 30,
               fontWeight: FontWeight.bold,
             ),
           ),
-          Container(height: 10.0),
-          Text(
-            "10 tarefas",
-            style: TextStyle(
-                fontSize: 18, fontWeight: FontWeight.w600, color: Colors.grey),
-          ),
-        ],
-      ),
     );
   }
 }
